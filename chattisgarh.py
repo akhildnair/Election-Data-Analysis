55681af5ce0c7005d34e7d1d4dafc1535d333415
akhildnair <103992465+akhildnair@users.noreply.github.com>
2024-06-30 18:21:01 +0530

    Add files via upload

---
+++ chattisgarh.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = 'https://results.eci.gov.in/PcResultGenJune2024/partywiseresult-S26.htm'
+
+response = requests.get(url)
+response.raise_for_status()  
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+
+resultstable =  soup.find('table', class_="table")
+headers_main = resultstable.find_all('th')
+headers = [title.text.strip() for title in headers_main][0:4]
+
+df = pd.DataFrame(columns=headers)
+
+column_data = resultstable.find_all('tr')
+for row in column_data[1:]:
+    row_data = row.find_all('td')
+    ird = [data.text.strip() for data in row_data]
+    if len(ird) == len(headers):  
+        length = len(df)
+        df.loc[length] = ird
+    
+
+print(df)
+
+df.to_excel('chattisgarh.xlsx', index=False)
